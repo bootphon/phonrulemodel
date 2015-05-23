cb8fbd1173dfb0044d15c740358b6607716f70c5
Maarten Versteegh <maartenversteegh@gmail.com>
2015-05-23 17:43:03 +0200

    end-to-end mfcc-to-bnf

--- train_acoustic_model.py
+++ train_acoustic_model.py
@@ -25,8 +25,10 @@ from sklearn.preprocessing import MinMaxScaler, StandardScaler
 import theano
 import theano.tensor as T
 import lasagne
-from lasagne.layers import DenseLayer, InputLayer, DropoutLayer
-from lasagne.nonlinearities import rectify, softmax, sigmoid
+from lasagne.layers import DenseLayer, InputLayer, DropoutLayer, \
+    get_all_layers, get_output
+from lasagne.nonlinearities import linear, leaky_rectify, softmax
+
 
 def float32(x):
     return np.cast['float32'](x)
@@ -52,7 +54,10 @@ def load_data(fname, test_prop=1/16, valid_prop=5/16, register='both',
     be tested without GPU
     """
     f = np.load(fname)
-    X, y, labels = f['X'], f['y'], f['labels']
+    X_, y_, labels = f['X'], f['y'], f['labels']
+    X = X_.copy()
+    y = y_.copy()
+
     if register in ['IDS', 'ADS']:
         sel_ixs = np.in1d(y, np.nonzero(labels[:, 1]==register))
         X = X[sel_ixs]
@@ -79,16 +84,19 @@ def load_data(fname, test_prop=1/16, valid_prop=5/16, register='both',
     nclasses = np.unique(y).shape[0]
     nfeatures = X.shape[1]
 
-    if testsubset:
-        X = X[1:17]
-        y = y[1:17]
-
     X_train, y_train, X_valid, y_valid, X_test, y_test = \
         train_valid_test_split(X, y,
                                test_prop=test_prop, valid_prop=valid_prop)
-    print X_train.shape, y_train.shape
-    print X_valid.shape, y_valid.shape
-    print X_test.shape, y_test.shape
+
+    if testsubset:
+        X = X_train[100:200]
+        y = y_train[100:200]
+        X_train = X_train[:100]
+        y_train = y_train[:100]
+        X_valid = X_valid[:10]
+        y_valid = y_valid[:10]
+        X_test = X_test[:50]
+        y_test = y_test[:50]
 
     return dict(
         X_train=theano.shared(X_train),
@@ -103,22 +111,23 @@ def load_data(fname, test_prop=1/16, valid_prop=5/16, register='both',
         input_dim=nfeatures,
         output_dim=nclasses,
         labels=labels
-    )
+    ), (X_, y_, labels)
 
 
 def build_model(input_dim, output_dim,
-                hidden_layers=(100,100),
-                batch_size=100, dropout=False, bottleneck = True, bsize = 50):
+                hidden_layers=(1000, 1000),
+                batch_size=100, dropout=False, bottleneck=True,
+                bottlenecksize=25):
     """
     If bottleneck = True, a bottleneck hiddenlayer of with bsize nodes is added
     """
     l_in = InputLayer(shape=(batch_size, input_dim))
     last = l_in
-    for size in hidden_layers:
-        l_hidden = DenseLayer(last, num_units=size,
-                              # nonlinearity=T.nnet.hard_sigmoid,
-                              nonlinearity=lasagne.nonlinearities.leaky_rectify,
-                              W=lasagne.init.GlorotUniform())
+    for ix, size in enumerate(hidden_layers[:-1]):
+        l_hidden = DenseLayer(
+            last, num_units=size,
+            nonlinearity=leaky_rectify,
+            W=lasagne.init.GlorotUniform())
         if dropout:
             l_dropout = DropoutLayer(l_hidden, p=0.5)
             last = l_dropout
@@ -126,24 +135,28 @@ def build_model(input_dim, output_dim,
             last = l_hidden
 
     if bottleneck:
-        l_bottleneck = DenseLayer(last, num_units=bsize,
-                              # nonlinearity=T.nnet.hard_sigmoid,
-                              nonlinearity=lasagne.nonlinearities.leaky_rectify,
-                              W=lasagne.init.GlorotUniform())
+        l_bottleneck = DenseLayer(
+            last, num_units=bottlenecksize,
+            name='bottleneck',
+            nonlinearity=linear,
+            W=lasagne.init.GlorotUniform())
         last = l_bottleneck
 
-    l_out = DenseLayer(last, num_units=output_dim, nonlinearity=softmax,
+    l_last_dense = DenseLayer(
+        last, num_units=hidden_layers[-1],
+        nonlinearity=leaky_rectify,
+        W=lasagne.init.GlorotUniform())
+    l_out = DenseLayer(l_last_dense, num_units=output_dim,
+                       nonlinearity=softmax,
                        W=lasagne.init.GlorotUniform())
-    return dict(
-        l_out = l_out,
-        last = last
-    )
+    return l_out
 
-def create_iter_func(dataset, dataset_all, output_layer,
-                     X_tensor_type=T.matrix,
-                     batch_size=300,
-                     learning_rate=0.01,
-                     momentum=0.9):
+
+def create_iter_funcs(dataset, output_layer,
+                      X_tensor_type=T.matrix,
+                      batch_size=300,
+                      learning_rate=0.01,
+                      momentum=0.9):
     """Create functions for training, validation and testing to iterate one
        epoch.
     """
@@ -161,7 +174,9 @@ def create_iter_func(dataset, dataset_all, output_layer,
                                    deterministic=True)
 
     pred = T.argmax(
-        output_layer.get_output(X_batch, deterministic=True), axis=1)
+        get_output(output_layer,
+                   X_batch,
+                   deterministic=True), axis=1)
     accuracy = T.mean(T.eq(pred, y_batch), dtype=theano.config.floatX)
 
     all_params = lasagne.layers.get_all_params(output_layer)
@@ -257,84 +272,36 @@ def train(iter_funcs, dataset, batch_size=300, test_every=100):
         }
 
 
-def load_all_data(fname, register='both',testsubset = False):
-    """
-        Creates dataset for generating new phone representations, without
-        seperating into different training, validation and testing tests
-        epoch.
-        If testsubset = True, load_data returns only a small dataset so code
-        can be tested without GPU
-    """
-    f = np.load(fname)
-    X, y, labels = f['X'], f['y'], f['labels']
-    if register in ['IDS', 'ADS']:
-        sel_ixs = np.in1d(y, np.nonzero(labels[:, 1]==register))
-        X = X[sel_ixs]
-        y = y[sel_ixs]
-    elif register == 'both':
-        ix2phone = dict(enumerate(labels[:, 0]))
-        phones = sorted(set(ix2phone.values()))
-        phone2newix = {p:ix for ix, p in enumerate(phones)}
-        y = np.array([phone2newix[ix2phone[i]] for i in y])
-        # phone2ix = {k: ix for ix, k in enumerate(labels[:, 0])}
+def get_bottleneck_features(network, X):
+    layers = get_all_layers(network)
+    bottleneck = [l for l in layers if l.name == 'bottleneck']
+    if len(bottleneck) == 0:
+        raise ValueError('network has no bottleneck')
     else:
-        raise ValueError('invalid option for register: {0}'.format(register))
+        bottleneck = bottleneck[0]
+    return lasagne.layers.get_output(bottleneck, X)
 
-    oldix2newix = {old_ix:new_ix for new_ix, old_ix in enumerate(np.unique(y))}
-    y = np.array([oldix2newix[i] for i in y])
 
-    print 'number of labels: {0}'.format(len(np.unique(y)))
+def main(npzfile, output, nlayers, nunits, nbottleneck,
+         num_epochs, batch_size, test, verbose):
 
-    X = StandardScaler().fit_transform(X)
-    X = MinMaxScaler(feature_range=(0,1)).fit_transform(X)
-    print X.min(), X.max()
-    X = X.astype(theano.config.floatX)
-    y = y.astype('int32')
-    nclasses = np.unique(y).shape[0]
-    nfeatures = X.shape[1]
-
-    if testsubset:
-        X = X[1:17]
-        y = y[1:17]
-
-    print X.shape, y.shape
-
-    return dict(
-        X=theano.shared(X),
-        y=theano.shared(y),
-        num_examples=X.shape[0],
-        input_dim=nfeatures,
-        output_dim=nclasses,
-        labels=labels
-    )
-
-def get_new_representations(iter_funcs, dataset_all, last):
-    """Run `dataset_all` through the model and save the second-to-last layer as
-       new phone representations
-    """
-    output = lasagne.layers.get_output(last, dataset_all['X']).eval()
-    labels = dataset_all['labels']
-    #print output
-    #print labels
-    return dict(
-        X = output,
-        y = labels
-    )
-
-if __name__ == '__main__':
-    num_epochs=10 #return back to 1000
-    batch_size=1 #return back to 1000
-    dataset = load_data('/Users/ingeborg/Desktop/mfcc.npz',
-                        valid_prop=4/16, test_prop=2/16, register='IDS',
-                        testsubset=True)
-    dataset_all = load_all_data('/Users/ingeborg/Desktop/mfcc.npz',
-                                register='IDS',testsubset = True)
+    dataset, (X, y, labels) = load_data(
+        npzfile,
+        valid_prop=4/16, test_prop=2/16,
+        register='both',
+        testsubset=test)
     output_layer = build_model(
-        input_dim=dataset['input_dim'], output_dim=dataset['output_dim'],
-        batch_size=batch_size, bottleneck = True, bsize = 50)
-    iter_funcs = create_iter_func(dataset, dataset_all, output_layer['l_out'],
-                                  batch_size=batch_size,
-                                  learning_rate=0.1, momentum=0.9)
+        input_dim=dataset['input_dim'],
+        output_dim=dataset['output_dim'],
+        hidden_layers=[nunits]*nlayers,
+        batch_size=batch_size,
+        bottleneck=True if nbottleneck > 0 else False,
+        bottlenecksize=nbottleneck)
+    iter_funcs = create_iter_funcs(
+        dataset, output_layer,
+        batch_size=batch_size,
+        learning_rate=0.1, momentum=0.9)
+
     test_every = 100
     now = time.time()
     try:
@@ -356,9 +323,69 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         pass
 
-    representations = get_new_representations(iter_funcs,
-                                              dataset_all,
-                                              output_layer['last'])
-    filename = '/Users/ingeborg/Desktop/reprs.npz'
-    np.savez(filename, X=representations['X'], y=representations['y'])
-        #scrhijf naar .npz file
+
+    X_bnf = get_bottleneck_features(output_layer, theano.shared(X)).eval()
+    np.savez(output, X=X_bnf, y=y, labels=labels)
+
+
+if __name__ == '__main__':
+    import argparse
+    def parse_args():
+        parser = argparse.ArgumentParser(
+            prog='train_acoustic_model.py',
+            formatter_class=argparse.RawDescriptionHelpFormatter,
+            description='train bnf acoustic model',
+            epilog="""Example usage:
+
+$ python train_acoustic_model.py /path/to/npzfile /path/to/output 100
+
+will train the model for 100 epochs on the data in npzfile. The input file
+needs to have X, y and labels keys.
+
+            """)
+        parser.add_argument('input', metavar='INPUT',
+                            nargs=1,
+                            help='npz input file')
+        parser.add_argument('output', metavar='OUTPUT',
+                            nargs=1,
+                            help='output file')
+        parser.add_argument('nlayers', metavar='NLAYERS',
+                            nargs=1,
+                            help='number of hidden layers')
+        parser.add_argument('nunits', metavar='NUNITS',
+                            nargs=1,
+                            help='number of units in hidden layers')
+        parser.add_argument('nbottleneck', metavar='NBOTTLENECK',
+                            nargs=1,
+                            help='number of bottleneck features')
+        parser.add_argument('nepochs', metavar='NEPOCHS',
+                            nargs=1,
+                            help='number of epochs to train')
+        parser.add_argument('batch_size', metavar='BATCH_SIZE',
+                            nargs=1,
+                            help='batch size.')
+        parser.add_argument('--test',
+                            action='store_true',
+                            dest='test',
+                            default=False,
+                            help='small datasets for testing')
+        parser.add_argument('-v', '--verbose',
+                            action='store_true',
+                            default=False,
+                            help='talk more')
+        return vars(parser.parse_args())
+
+
+    args = parse_args()
+    num_epochs = int(args['nepochs'][0])
+    npzfile = args['input'][0]
+    output = args['output'][0]
+    batch_size = int(args['batch_size'][0])
+    nbottleneck = int(args['nbottleneck'][0])
+    nunits = int(args['nunits'][0])
+    nlayers = int(args['nlayers'][0])
+    verbose = args['verbose']
+    test = args['test']
+
+    main(npzfile, output, nlayers, nunits, nbottleneck,
+         num_epochs, batch_size, test, verbose)
