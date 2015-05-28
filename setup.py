329070ff25ccd759a92aafacbf607e0f1543606f
Maarten Versteegh <maartenversteegh@gmail.com>
2015-05-28 16:49:02 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(
+    name="phonrulemodel",
+    version="0.1.0",
+    url="https://github.com/bootphon/phonrulemodel",
+
+    author="Maarten Versteegh",
+    author_email="maartenversteegh@gmail.com",
+
+    description="",
+    long_description="",
+
+    packages=setuptools.find_packages(),
+
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
