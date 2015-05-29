dadf444a90ce7200a430bca8754bd6c742975a1a
Ingeborg <ingeborg.roete@gmail.com>
2015-05-29 12:14:24 +0200

    Add function to read in samples, todo: fix bug

--- bottleneck_features.py
+++ bottleneck_features.py
@@ -13,6 +13,8 @@
 # ------------------------------------
 """bottleneck_features:
 
+3. train een binaire classifier 
+op de bottleneck features.
 """
 
 from __future__ import division
@@ -22,6 +24,9 @@ from lasagne.layers import \
 
 from dnn import load_model
 
+import numpy as np
+from sklearn import svm
+
 def get_bottleneck_features(network, X):
     layers = get_all_layers(network)
     bottleneck = [l for l in layers if l.name == 'bottleneck']
@@ -29,11 +34,35 @@ def get_bottleneck_features(network, X):
         raise ValueError('network has no bottleneck')
     else:
         bottleneck = bottleneck[0]
-    return get_output(bottleneck, X)
+    bfeatures = get_output(bottleneck, X)
+    #print X.shape
+    #print bfeatures.eval().shape
+    return bfeatures
+
+def train_SVM(X, y):
+    clf = svm.SVC()
+    clf.fit(X, y)  
 
 if __name__ == '__main__':
     import sys
-    fname = sys.argv[1]
-    model = load_model(fname)
+    #fname = sys.argv[1]
+
+    fname1 = "/Users/ingeborg/model1/model_0.pkl"
+    fname2 = "/Users/ingeborg/model2/model_0.pkl"
+    fsamples = "/Users/ingeborg/Desktop/output.npz"
+    dataset = np.load(fsamples)
+    X, y, labels = dataset['X'], dataset['y'], dataset['labels']
+
+    model = load_model(fname1)
     for layer in get_all_layers(model):
         print layer.name, get_output_shape(layer)
+    b_features1 = get_bottleneck_features(model, X)
+
+    #TODO: fix bug: ValueError: setting an array element with a sequence.
+    #train_SVM(b_features1, y)
+
+    model = load_model(fname2)
+    for layer in get_all_layers(model):
+        print layer.name, get_output_shape(layer)
+    b_features2 = get_bottleneck_features(model, X)
+    #train_SVM(b_features1, y)
